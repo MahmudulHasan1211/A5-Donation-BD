4b212f5373faa2b321d57731c00d0843fae133fc
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-24 11:28:48 +0600

    first card added

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'custom-green': '#b4f461',  // Custom green color
+      },
+    },
+  },
+  plugins: [],
+}
